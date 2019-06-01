b311720d9b857af4f9ab77d2f6f1dfdb53c1a6bf
Rajesh <rkannapa@cisco.com>
2019-06-01 16:17:36 -0700

    Initial draft

---
+++ scripts.js
@@ -0,0 +1,45 @@
+var main = function(){
+	(function blink() {
+    $('.blink_me').fadeOut(850).fadeIn(850, blink);
+  })();
+  $('.barvalue').hide();
+  $('.barpercent').hide();
+  // Add smooth scrolling to all links
+
+$('[id="submit"]').on('click',function(event){
+  event.preventDefault();
+  $('[id = "InputName"]').val("");
+  $('[id = "InputEmail"]').val("");
+  $('[id = "InputSubject"]').val("");
+  $('[id = "InputMessage"]').val("");
+});
+$("a").on('click', function(event) {
+
+  if($(this).text().trim() != "Previous" && $(this).text().trim() != "Next" && $(this).attr("class") != "icon" && $(this).attr("class") != "btn btn-warning" && $(this).attr("class") != "btn btn-info pull-right"){
+// Prevent default anchor click behavior
+
+  event.preventDefault();
+
+  // Store hash
+  var hash = this.hash;
+
+  // Using jQuery's animate() method to add smooth page scroll
+  // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top
+  }, 800, function(){
+
+    // Add hash (#) to URL when done scrolling (default click behavior)
+    window.location.hash = hash;
+  });
+  }
+});
+
+}
+
+
+
+
+
+
+$(document).ready(main);
